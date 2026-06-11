c845ed3f016f436ef320d68abe08e2bc3c94f267
KurtChristianLawas02 <lawaskc02@gmai.com>
2026-06-11 18:58:47 +0800

    Fix Aiven MySQL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,41 @@
+
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+pool.getConnection()
+  .then(conn => {
+    console.log('✅ MySQL connected successfully');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('❌ MySQL connection failed:', err.message);
+  });
+
+module.exports = pool;
+
+
+
+
+
+
+
+
+
+/*
+
 // backend/config/db.js
 const mysql = require('mysql2/promise');
 
@@ -20,4 +58,5 @@ pool.getConnection()
     console.error('❌ MySQL connection failed:', err.message);
   });
 
-module.exports = pool;
+module.exports = pool; *
+/
